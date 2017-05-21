7d2d4e6f3051676167e3f1c1e7bb53bf492e4641
Varit Assavavisidchai <varit.as@ku.th>
2017-05-21 23:27:27 +0700

    error test

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,10 @@
-package UI.application;
+package application;
+
+import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.LoadException;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,7 +14,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(ClassLoader.getSystemResource("UI/application/Piano.fxml"));
+			Parent root = FXMLLoader.load(ClassLoader.getSystemResource("application/Piano.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
