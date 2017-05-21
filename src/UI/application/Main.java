f013d731883182eea1fcab12d7d2f1dd257668a6
Non Puthikanon <non.pu@ku.th>
2017-05-21 15:30:51 +0700

    after merge

--- src/UI/application/Main.java
+++ src/UI/application/Main.java
@@ -1,5 +1,5 @@
-package application;
-	
+package UI.application;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -7,21 +7,20 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(ClassLoader.getSystemResource("application/Piano.fxml"));
+			Parent root = FXMLLoader.load(ClassLoader.getSystemResource("UI/application/Piano.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
